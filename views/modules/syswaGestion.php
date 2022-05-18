698660eb99eda37d1b0a3f1340df7ed9c99fb5af
carlos capacho <carlos.gomes349@gmail.com>
2022-05-17 20:39:57 -0400

    add changes

--- views/modules/syswaGestion.php
+++ views/modules/syswaGestion.php
@@ -5,44 +5,77 @@
       <section class="breadcrumbs-custom bg-image context-dark" style="background-image: url(views/images/jumbotron-1-1919x953.jpg);">
         <div class="container">
           <h2 class="breadcrumbs-custom-title">Syswa Gestión</h2>
-          <div class="breadcrumbs-custom-subtitle">Consulta nuestros contactos o escríbenos un mensaje</div>
+          <div class="breadcrumbs-custom-subtitle">Continua para informarte sobre nuestro sistema de gestión</div>
         </div>
       </section>
       <!-- Blurb minimal-->
-      <section class="section section-md bg-default">
+      <section class="section bg-gray-700 context-dark section-relative">
         <div class="container">
-          <div class="row row-40">
-            <div class="col-sm-6 col-md-4">
-              <div class="box-lined">
-                <div class="box-lined-body">
-                  <h6 class="box-lined-title">Correo & Teléfono</h6>
-                  <ul class="box-lined-list">
-                    <li><a class="big link-default" href="mailto:info@syswa.net">info@syswa.net</a></li>
-                    <li><a class="big link-default">9 4053 8571</a></li>
-                    <li><a class="big link-default">412 4053 857</a></li>
-                  </ul>
-                </div>
+          <div class="row">
+            <div class="col-md-5 mt-4 text-block-1 wow fadeInLeft">
+              <div class="section-sm section-sm-3">
+                <h5>GESTIONA TU NEGOCIO</h5>
+                <p>Sistema de agendamiento, diseñado para cubrir todas tus necesidades, pensado para tu pequeño, mediano o gran negocio de servicios, lleva el control con más de 10 secciones administrativas, y un montón de funcionalidades pensadas solo para ti.</p>
+                <div class="button-wrap"><a class="button button-default button-invariable" href="#">Me interesa</a></div>
               </div>
             </div>
-            <div class="col-sm-6 col-md-4">
-              <div class="box-lined">
-                <div class="box-lined-body">
-                  <h6 class="box-lined-title">Ubicaciones principales</h6>
-                  <ul class="box-lined-list">
-                    <li class="big">Chile, Santiago</li>
-                    <li class="big">Aragua, Venezuela</li>
-                  </ul>
-                </div>
+          </div>
+        </div>
+        <!-- Swiper-->
+        <div class="swiper-section">
+          <div class="swiper-container swiper-slider swiper-slider-1" data-next=".custom-swiper-button-next" data-prev=".custom-swiper-button-prev" data-loop="true">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" data-slide-bg="views/images/syswa1.png"></div>
+              <div class="swiper-slide" data-slide-bg="views/images/syswa2.png"></div>
+              <div class="swiper-slide" data-slide-bg="views/images/reportesSyswa.png"></div>
+              <div class="swiper-slide" data-slide-bg="views/images/procesar.png"></div>
+              <div class="swiper-slide" data-slide-bg="views/images/gastos.png"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <!-- Swiper Navigation-->
+          </div>
+          <div class="custom-swiper-button-wrap">
+            <div class="custom-swiper-button-next mdi mdi-arrow-right"></div>
+            <div class="custom-swiper-button-prev mdi mdi-arrow-left"></div>
+          </div>
+        </div>
+      </section>
+      <!-- Gif -->
+      <section class="section section-lg bg-gray-100 section-lined">
+        <div class="container container-lined">
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row row-40 justify-content-between">
+            <div class="col-lg-7 col-xl-8">
+              <div class="row no-gutters">
+                <img src="views/images/metricas.gif" alt="" width="100%" height="1050"/>
               </div>
             </div>
-            <div class="col-sm-12 col-md-4">
-              <div class="box-lined">
-                <div class="box-lined-body">
-                  <h6 class="box-lined-title">Redes sociales</h6>
-                  <ul class="list-inline list-inline-sm">
-                    <li><a class="icon icon-sm icon-bordered link-default mdi mdi-facebook" href="#"></a></li>
-                    <li><a class="icon icon-sm icon-bordered link-default mdi mdi-instagram" href="#"></a></li>
-                    <li><a class="icon icon-sm icon-bordered link-default mdi mdi-linkedin" href="#"></a></li>
+            <div class="col-lg-5 col-xl-4">
+              <div class="row row-30 row-xxl-55 justify-content-end">
+                <div class="col-sm-6 col-lg-9 col-xl-10 text-lg-right">
+                  <img src="views/images/syswaLetras.png" alt=""/>
+                  <div class="big-text">Tenemos un montón de funcionalidades que harán que tu negocio sea fácil de administrar.</div>
+                  <div class="divider divider-2 d-none d-lg-block"></div>
+                </div>
+                <div class="col-sm-6 col-lg-8 col-xl-7">
+                  <div class="counter-minimal">
+                    <div class="counter-left">
+                      <div class="counter">5</div>
+                    </div>
+                    <div class="counter-right">
+                      <div class="postfix">+</div>
+                      <div class="title">Clientes contentos</div>
+                    </div>
+                  </div>
+                  <ul class="list-marked list-marked-big">
+                    <li>Equipo profesional con más de 5 años de experiencia</li>
+                    <li>Soporte técnico</li>
                   </ul>
                 </div>
               </div>
@@ -50,47 +83,105 @@
           </div>
         </div>
       </section>
-      <!-- Get in touch with us-->
-      <section class="section section-md bg-default">
+      <!-- pdf -->
+      <section class="section section-sm bg-gray-700 text-center text-sm-left">
         <div class="container">
-          <h4>PÓNGASE EN CONTACTO CON NOSOTROS</h4>
-          <p class="big">Llenando este formulario, se pondria en contacto directo con nuestro equipo.</p>
-          <!-- RD Mailform-->
-          <form class="rd-form rd-mailform form-boxed" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
-            <div class="row row-50">
-              <div class="col-lg-4">
-                <div class="form-wrap form-wrap-icon">
-                  <div class="form-icon mdi mdi-account-outline"></div>
-                  <input class="form-input" id="contact-name" type="text" name="name" data-constraints="@Required">
-                  <label class="form-label" for="contact-name">Nombre</label>
+          <div class="row">
+            <div class="col-md-10">
+              <h4>Consulta nuestro manual de uso</h4>
+            </div>
+            <div class="col-md-2">
+              <div class="button-wrap"><a class="button button-default button-invariable" href="#">Descargar</a></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="breadcrumbs-custom bg-image context-dark section-90vh section-fullheight" style="background-image: url(views/images/keywords.png);"><div class="section-fullheight-inner section-md"></div>
+      </section>
+      <!-- Planes -->
+      <section class="section section-sm bg-gray-700 section-lined">
+        <h4 class="title-bold text-center mb-2">Planes SYSWA GESTIÓN</h4>
+        <hr class="w-50 mt-3 mb-5 bg-white">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="box-shadow-pricing" style="background-color: rgb(255, 255, 255);">
+                <div class="w-100 py-4 style-header-princing" style="background-color: rgba(0, 0, 0, 0.95);">
+                  <h5 class="text-center text-white">EMPRENDEDOR</h5>
                 </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="form-wrap form-wrap-icon">
-                  <div class="form-icon mdi mdi-email-outline"></div>
-                  <input class="form-input" id="contact-email" type="email" name="email" data-constraints="@Email @Required">
-                  <label class="form-label" for="contact-email">Correo</label>
+                <div class="w-100 py-2 color-text" style="background-color: rgb(255, 255, 255);">
+                  <h4 class="text-center mt-2">29.990 CLP/Mes</h4>
+                  <hr class="w-50 mt-3 mb-3">
+                  <p class="text-center font-weight-bolder">2 Empleados</p>
+                  <p class="text-center font-weight-bolder">Única sucursal</p>
+                  <p class="text-center font-weight-bolder">Procesar venta</p>
+                  <p class="text-center font-weight-bolder">Sección agendamiento</p>
+                  <p class="text-center font-weight-bolder">Sección gestión de ventas</p>
+                  <p class="text-center font-weight-bolder">Sección servicios</p>
+                  <p class="text-center font-weight-bolder">Sección clientes</p>
+                  <p class="text-center font-weight-bolder">Sección caja</p>
+                  <p class="text-center font-weight-bolder">Notificaciones</p>
+                  <br><br><br><br>
+                  <hr class="w-50 mb-3 mt-4">
+                  <center>
+                    <div class="button-wrap"><a class="button button-default button-invariable" href="#">Lo quiero</a></div>
+                  </center>
                 </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="form-wrap form-wrap-icon">
-                  <div class="form-icon mdi mdi-phone"></div>
-                  <input class="form-input" id="contact-phone" type="text" name="phone" data-constraints="@Numeric">
-                  <label class="form-label" for="contact-phone">Teléfono</label>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-wrap form-wrap-icon">
-                  <div class="form-icon mdi mdi-message-outline"></div>
-                  <label class="form-label" for="contact-message">Mensaje</label>
-                  <textarea class="form-input" id="contact-message" name="message" data-constraints="@Required"></textarea>
-                </div>
-              </div>
-              <div class="col-md-12">
-                <button class="button button-default" type="submit">Enviar</button>
               </div>
             </div>
-          </form>
+            <div class="col-md-4">
+              <div class="box-shadow-pricing" style="background-color: rgb(255, 255, 255);">
+                <div class="w-100 py-4 style-header-princing" style="background-color: rgba(0, 0, 0, 0.95);">
+                  <h5 class="text-center text-white">BÁSICO</h5>
+                </div>
+                <div class="w-100 py-2 color-text" style="background-color: rgb(255, 255, 255);">
+                  <h4 class="text-center mt-2">39.990 CLP/Mes</h4>
+                  <hr class="w-50 mt-3 mb-3">
+                  <p class="text-center font-weight-bolder">10 Empleados</p>
+                  <p class="text-center font-weight-bolder">Hasta 2 sucursales</p>
+                  <p class="text-center font-weight-bolder">Procesar venta</p>
+                  <p class="text-center font-weight-bolder">Sección agendamiento</p>
+                  <p class="text-center font-weight-bolder">Sección gestión de ventas</p>
+                  <p class="text-center font-weight-bolder">Sección servicios</p>
+                  <p class="text-center font-weight-bolder">Sección clientes</p>
+                  <p class="text-center font-weight-bolder">Sección caja</p>
+                  <p class="text-center font-weight-bolder">Notificaciones</p>
+                  <br><br><br><br><br>
+                  <hr class="w-50 mt-3 mb-3">
+                  <center>
+                    <div class="button-wrap"><a class="button button-default button-invariable" href="#">Lo quiero</a></div>
+                  </center>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="box-shadow-pricing" style="background-color: rgb(255, 255, 255);">
+                <div class="w-100 py-4 style-header-princing" style="background-color: rgba(0, 0, 0, 0.95);">
+                  <h5 class="text-center text-white">PRO</h5>
+                </div>
+                <div class="w-100 py-2 color-text" style="background-color: rgb(255, 255, 255);">
+                  <h4 class="text-center mt-2">49.990 CLP/Mes</h4>
+                  <hr class="w-50 mt-3 mb-3">
+                  <p class="text-center font-weight-bolder">Empleados ilimitados</p>
+                  <p class="text-center font-weight-bolder">Sucursales ilimitadas</p>
+                  <p class="text-center font-weight-bolder">Procesar venta</p>
+                  <p class="text-center font-weight-bolder">Sección agendamiento</p>
+                  <p class="text-center font-weight-bolder">Sección gestión de ventas</p>
+                  <p class="text-center font-weight-bolder">Sección servicios</p>
+                  <p class="text-center font-weight-bolder">Sección clientes</p>
+                  <p class="text-center font-weight-bolder">Sección caja</p>
+                  <p class="text-center font-weight-bolder">Sección reportes</p>
+                  <p class="text-center font-weight-bolder">Sección gastos</p>
+                  <p class="text-center font-weight-bolder">Sección inventario</p>
+                  <p class="text-center font-weight-bolder">Notificaciones</p>
+                  <hr class="w-50 mt-3 mb-3">
+                  <center>
+                    <div class="button-wrap"><a class="button button-default button-invariable" href="#">Lo quiero</a></div>
+                  </center>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <!-- Page Footer-->
